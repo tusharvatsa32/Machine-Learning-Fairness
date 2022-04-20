6805fce08fef9525fd3e806611f79549ac52983e
Tushar <tvatsa@andrew.cmu.edu>
2022-04-20 00:58:07 -0400

    Adding univariate_analysis.py and setup.py

---
+++ setup.py
@@ -0,0 +1,7 @@
+import subprocess
+import os,stat,sys
+import shutil
+from shutil import copyfile
+from sys import exit
+
+subprocess.check_call([sys.executable,'-m','pip','install','seaborn'])
--- univariate_analysis.py
+++ univariate_analysis.py
@@ -1,6 +1,9 @@
 from dataset import read_dataframe
 import pandas as pd
+import numpy as np
+import seaborn as sns
 import matplotlib.pyplot as plt
+import warnings
 
 def plotObjColumnDistribution(df : pd.DataFrame, nGraphShown : int, nGraphPerRow : int):
     """
@@ -22,6 +25,9 @@ def plotObjColumnDistribution(df : pd.DataFrame, nGraphShown : int, nGraphPerRow
 
     df = df[[col for col in df.select_dtypes(["object", "category"])]] # pick columns with object type
 
+    warnings.filterwarnings('ignore')
+    cmap=sns.color_palette('Blues_r')
+
     nRow, nCol = df.shape
     columnNames = list(df)
     nGraphRow = (nCol + nGraphPerRow - 1) / nGraphPerRow
@@ -38,6 +44,37 @@ def plotObjColumnDistribution(df : pd.DataFrame, nGraphShown : int, nGraphPerRow
         plt.xticks(rotation = 90)
         plt.title(f'{columnNames[i]} (column {i})')
 
+    # Show the plot
+    plt.tight_layout()
+    plt.show()
+
+def plotNumColumnDistribution(df : pd.DataFrame, nGraphPerRow : int):
+    """
+    A function to visualize the numerical variables and their distribution.
+
+    Parameters
+    ----------
+    df
+        The dataframe corresponding to the german credit dataset
+
+    nGraphPerRow
+        The number of graphs in each row
+    """
+    print(f'=== Distribution of features with number values ===')
+
+    df = df[[col for col in df.select_dtypes(np.number)]] # pick columns with bumber type
+
+    nRow, nCol = df.shape
+    columnNames = list(df)
+    nGraphRow = (nCol + nGraphPerRow - 1) / nGraphPerRow
+
+    plt.figure(num = None, figsize = (6 * nGraphPerRow, 3 * nGraphRow))
+
+    for i, col in enumerate(columnNames):
+        # Plot distribution
+        plt.subplot(int(nGraphRow), nGraphPerRow, i + 1)
+        sns.distplot(df[col], color='blue')
+        plt.title(f'Distribution of {col}')
     # Show the plot
     plt.tight_layout()
     plt.show()
